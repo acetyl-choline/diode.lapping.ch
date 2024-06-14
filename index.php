31354c216f7846a76c35d97e999a7880e61c6d73
acetyl-choline <46597015+acetyl-choline@users.noreply.github.com>
2024-06-14 20:13:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -74,11 +74,12 @@
 
 			// Save values
 			file_put_contents(detector.log, $detector);
-			file_put_contents(wlunit.log, wlunit);
-			file_put_contents(wavelength.log, wavelength);
-			file_put_contents(runit.log, runit);
-			file_put_contents(vunit.log, vunit);
-			file_put_contents(voltage.log, voltage);
+			file_put_contents(wlunit.log, $wlunit);
+			file_put_contents(wavelength.log, $wavelength);
+			file_put_contents(runit.log, $runit);
+			file_put_contents(resistance.log, $resistance);
+			file_put_contents(vunit.log, $vunit);
+			file_put_contents(voltage.log, $voltage);
 			
 			// Perform linear interpolation
 			$i = 0;
